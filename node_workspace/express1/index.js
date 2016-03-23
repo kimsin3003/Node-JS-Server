a7f825af1743b48d1f177df98da3986bdc83ee0a
taewoo kim <kimsin3003@naver.com>
2016-03-23 23:05:32 +0900

    hw1 ing..

--- node_workspace/express1/index.js
+++ node_workspace/express1/index.js
@@ -3,6 +3,14 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+app.set('view engine', 'jade');
+
+var handlebars = require('express-handlebars').create();
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars')
+
+
+
 app.get('/', function(req, res){ //route 메서드들은 위에서부터 순서대로 검사하다가 먼저걸리는 것에 들어간다.
   res.type('text/plain')
   res.send('Express Index');
@@ -13,6 +21,16 @@ app.get('/about', function(req, res){
   res.send('Express About');
 });
 
+app.get('/jade', function(req, res){
+  res.type('text/html');
+  res.render('test', { title: 'Hey', message: 'This is jade template'});
+});
+
+app.get('/handlebars', function(req, res){
+  res.type('text/html');
+  res.render('test', {title: 'Hey', message: 'This is handlebars template'})
+});
+
 app.use(function(req,res){//반드시 get 밑에 있어야한다.
   res.type('text/plain');
   res.status('404');
@@ -20,6 +38,7 @@ app.use(function(req,res){//반드시 get 밑에 있어야한다.
 });
 
 app.use(function(err, req, res,next){
+  console.log(req.url);
   console.error(err.stack);
   res.type('text/plain');
   res.status('500');
